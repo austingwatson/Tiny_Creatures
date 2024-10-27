c77b5108989e4b0587af55c74d948723bd937da8
Austin Watson <austingwatson@hotmail.com>
2024-10-27 12:50:39 -0500

    base bestiary done

--- scripts/ui/bestiary.gd
+++ scripts/ui/bestiary.gd
@@ -1,6 +1,10 @@
 extends Control
 
 export var names = ["", "", "", ""]
+export var purple_icon: Texture
+export var green_icon: Texture
+export var red_icon: Texture
+export var yellow_icon: Texture
 export var descriptions = ["", "", "", ""]
 
 var creature = Juice.Creature.SLUG
@@ -8,10 +12,13 @@ var creature = Juice.Creature.SLUG
 onready var book = $Book/Background
 onready var hover_text = $Book/HoverText
 onready var pages = $Pages
-onready var page_name = $Pages/Name
-onready var page_text = $Pages/Text
+onready var page_name = $Pages/VBoxContainer/NameBackground/Name
+onready var page_text = $Pages/VBoxContainer/TextBackground/Text
 onready var prev_page = $Pages/PrevPage
 onready var next_page = $Pages/NextPage
+onready var icon = $Pages/VBoxContainer/IconBackground/Icon
+onready var close_label = $Pages/HBoxContainer/Label
+onready var animation_player = $AnimationPlayer
 
 
 func _ready():
@@ -22,11 +29,22 @@ func set_page():
 	page_name.text = names[creature]
 	page_text.text = descriptions[creature]
 	
+	match creature:
+		Juice.Creature.SLUG:
+			icon.texture = purple_icon
+		Juice.Creature.SLIME:
+			icon.texture = green_icon
+		Juice.Creature.AMOEBA:
+			icon.texture = red_icon
+		Juice.Creature.SCARY_BOY:
+			icon.texture = yellow_icon
+	
 
 func _on_Book_pressed():
 	book.visible = false
 	hover_text.visible = false
 	pages.visible = true
+	animation_player.play("open_book")
 
 
 func _on_Background_mouse_entered():
@@ -39,7 +57,8 @@ func _on_Background_mouse_exited():
 
 func _on_Close_pressed():
 	book.visible = true
-	pages.visible = false
+	#pages.visible = false
+	animation_player.play_backwards("open_book")
 
 
 func _on_PrevPage_pressed():
@@ -58,3 +77,11 @@ func _on_NextPage_pressed():
 		creature = Juice.Creature.size() - 1
 		next_page.visible = false
 	set_page()
+
+
+func _on_Close_mouse_entered():
+	close_label.visible = true
+
+
+func _on_Close_mouse_exited():
+	close_label.visible = false
